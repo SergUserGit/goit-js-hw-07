6c273c0cdb914cd2242385a0017b11d45faeeade
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-16 10:53:00 +0200

    new changes
    new changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+const strMarkup = galleryItems.map((el) => {
+  const newElem =
+    '<div class="gallery__item">' +
+    '<a class="gallery__link" href="' +
+    el.original +
+    '">' +
+    "<img " +
+    'class="gallery__image"' +
+    ' src="' +
+    el.preview +
+    '"' +
+    ' data-source="' +
+    el.original +
+    '"' +
+    ' alt="' +
+    el.description +
+    '"' +
+    "/>" +
+    "</a>" +
+    "</div> ";
+
+  return newElem;
+});
+
+const elemGallery = document.querySelector(".gallery");
+elemGallery.addEventListener("click", onGalleryDivClick);
+
+elemGallery.insertAdjacentHTML("afterbegin", strMarkup.join(""));
+
+function onGalleryDivClick(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  console.log(event.target.dataset.source);
+}
