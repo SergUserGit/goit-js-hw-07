14f16c45dea969e348cacbcce9ff97b9d958009e
Sergii Podriezienko <podreenko1986@gmail.com>
2023-01-16 12:11:50 +0200

    part 2 work
    part 2 work

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const strMarkup = galleryItems.map((el) => {
+  const newElem =
+    "<li>" +
+    '<a class="gallery__item"' +
+    'href = "' +
+    el.original +
+    '">' +
+    '<img class="gallery__image" src="' +
+    el.preview +
+    '" alt="' +
+    el.description +
+    '" />' +
+    "</a>" +
+    "</li>";
+  return newElem;
+});
+
+const elemGallery = document.querySelector(".gallery");
+
+elemGallery.insertAdjacentHTML("afterbegin", strMarkup.join(""));
